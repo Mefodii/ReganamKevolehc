f51a34145980cad8dce3436be61a989ca25e2ce1
Mefodii <mefodii04@gmail.com>
2020-10-19 21:53:55 +0300

    Tailwind init

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -4,55 +4,26 @@ import { NavLink, withRouter } from "react-router-dom";
 export class Header extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarTogglerDemo01"
-          aria-controls="navbarTogglerDemo01"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-          <NavLink to="/" className="navbar-brand">
-            Kevolehc
+      <nav className="text-gray-200" id="site-header">
+        <ul>
+          <li>
+            <NavLink to="/">Kevolehc</NavLink>
+          </li>
+          <li>
+            <NavLink to="/watchio">WatchIO</NavLink>
+            <div>
+              <NavLink to="/watchio/movies">Movies</NavLink>
+              <NavLink to="/watchio/serials">Serials</NavLink>
+              <NavLink to="/watchio/anime">Anime</NavLink>
+            </div>
+          </li>
+          <NavLink to="/gamio">
+            <li>GameIO</li>
           </NavLink>
-          <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                id="navbarDropdown"
-                role="button"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                WatchIO
-              </a>
-              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <NavLink to="/watchio/movies" className="dropdown-item">
-                  Movies
-                </NavLink>
-                <NavLink to="/watchio/serials" className="dropdown-item">
-                  Serials
-                </NavLink>
-                <NavLink to="/watchio/anime" className="dropdown-item">
-                  Anime
-                </NavLink>
-              </div>
-            </li>
-            <NavLink to="/gamio" className="nav-link">
-              <li className="nav-item">GameIO</li>
-            </NavLink>
-            <NavLink to="/readio" className="nav-link">
-              <li className="nav-item">ReadIO</li>
-            </NavLink>
-          </ul>
-        </div>
+          <NavLink to="/readio">
+            <li>ReadIO</li>
+          </NavLink>
+        </ul>
       </nav>
     );
   }
