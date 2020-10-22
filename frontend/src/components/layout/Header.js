154c8458306ddffe367b879f5814078a364fc5af
Mefodii <mefodii04@gmail.com>
2020-10-22 08:34:05 +0300

    Navbar draft

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -4,25 +4,39 @@ import { NavLink, withRouter } from "react-router-dom";
 export class Header extends Component {
   render() {
     return (
-      <nav className="text-gray-200" id="site-header">
-        <ul>
-          <li>
-            <NavLink to="/">Kevolehc</NavLink>
+      <nav className="text-gray-200 p-5 shadow-md bg-gray-800" id="site-header">
+        <ul className="flex">
+          <li className="mr-5 text-lg font-bold">
+            <NavLink to="/">:Kevolehc</NavLink>
           </li>
-          <li>
+          <li className="relative mr-5">
             <NavLink to="/watchio">WatchIO</NavLink>
-            <div>
+            <svg
+              className="w-5 inline transform hover:rotate-180"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M19 9l-7 7-7-7"
+              ></path>
+            </svg>
+            <div className="hidden">
               <NavLink to="/watchio/movies">Movies</NavLink>
               <NavLink to="/watchio/serials">Serials</NavLink>
               <NavLink to="/watchio/anime">Anime</NavLink>
             </div>
           </li>
-          <NavLink to="/gamio">
-            <li>GameIO</li>
-          </NavLink>
-          <NavLink to="/readio">
-            <li>ReadIO</li>
-          </NavLink>
+          <li className="mr-5">
+            <NavLink to="/gamio">GameIO</NavLink>
+          </li>
+          <li>
+            <NavLink to="/readio">ReadIO</NavLink>
+          </li>
         </ul>
       </nav>
     );
